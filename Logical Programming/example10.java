a8d5c67105a90f6a67fed6b1e6fa66dfe253edfe
Harish0528 <87277530+Harish0528@users.noreply.github.com>
2021-10-27 15:51:59 +0530

    Update example10.java

--- Logical Programming/example10.java
+++ Logical Programming/example10.java
@@ -1,3 +1,4 @@
+//10. Write a java program to check whether a character is uppercase or lowercase alphabet.
 import java.util.Scanner;
 public class example10 {
 	public static void main(String[] args) {
