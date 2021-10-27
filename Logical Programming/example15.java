781a615ddd9d6e24e9f7955596689f1117df00f4
Harish0528 <87277530+Harish0528@users.noreply.github.com>
2021-10-27 15:55:59 +0530

    Update example15.java

--- Logical Programming/example15.java
+++ Logical Programming/example15.java
@@ -1,3 +1,9 @@
+// 15. Write a java program to input electricity unit charges and calculate total     electricity bill according to the given condition:
+//     For first 50 units Rs. 0.50/unit
+//     For next 100 units Rs. 0.75/unit
+//     For next 100 units Rs. 1.20/unit
+//     For unit above 250 Rs. 1.50/unit
+//     An additional surcharge of 20% is added to the bill
 import java.util.Scanner;
 public class example15{
     public static void main(String[] args){
