6dd31b4c2d7f18bca7de470b923ea7e536cf6dae
Harish0528 <87277530+Harish0528@users.noreply.github.com>
2021-10-27 15:54:45 +0530

    Update example14.java

--- Logical Programming/example14.java
+++ Logical Programming/example14.java
@@ -1,3 +1,7 @@
+// 14. Write a java program to input basic salary of an employee and calculate its Gross     salary according to following:
+//     Basic Salary <= 10000 : HRA = 20%, DA = 80%
+//     Basic Salary <= 20000 : HRA = 25%, DA = 90%
+//     Basic Salary > 20000 : HRA = 30%, DA = 95%
 import java.util.Scanner;
 public class example14 {
 	public static void main(String[] args) {
